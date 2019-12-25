1c77628156f5ac46c6a210747ae22cb90488a639
Denis <36458874+cr0manty@users.noreply.github.com>
2019-12-26 00:42:45 +0200

    delete unused

--- lib/colors.dart
+++ lib/colors.dart
@@ -17,19 +17,4 @@ const MaterialColor primaryBlack = MaterialColor(
 );
 const int _blackPrimaryValue = 0xFF000000;
 
-//const MaterialColor lightGrey = MaterialColor(
-//  _lightGreyPrimaryValue,
-//  <int, Color>{
-//       50: Color(0xFFdedede),
-//      100: Color(0xFFa5a5a5),
-//      200: Color(0xFF969696),
-//      300: Color(0xFF888888),
-//      400: Color(0xFF757575),
-//      500: Color(_lightGreyPrimaryValue),
-//      600: Color(0xFF505050),
-//      700: Color(0xFF424242),
-//      800: Color(0xFF2d2c2c),
-//      900: Color(0xFF1b1b1b),
-//    }
-//);
 const Color lightGrey = Color(0xFFdedede);
