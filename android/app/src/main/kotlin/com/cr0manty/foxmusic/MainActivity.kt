8cba24345494b2593f1b6b13ec9199fc875ba7e9
cr0manty <36458874+cr0manty@users.noreply.github.com>
2020-03-07 22:44:01 +0200

    new player

---
+++ android/app/src/main/kotlin/com/cr0manty/foxmusic/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cr0manty.foxmusic
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
